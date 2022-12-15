97e08ccc6474ad85b6df12266cb855b3e812c1e1
Jeehwan Cho <cjhpaul@hotmail.com>
2022-12-15 13:44:20 -0500

    add bug fixes and general enhancements

--- main.py
+++ main.py
@@ -1,92 +1,218 @@
+import os
 import csv
 from PyPDF2 import PdfReader
 from sklearn import tree
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+import graphviz
 
+# features based on: https://fieldwire.atlassian.net/wiki/spaces/ENGINEERIN/pages/1298890843/Specification+File+Automation+-+section+detection
+# features_dictionary[{page_num}-{y}] = {'x': 123, 'y': 321, 'text': 'foobar', 'page_number': 123}
+# features have to be float
 class Specification:
-  def __init__(self, pdf_file_path):
-    self.yToFeatures = {}
-    self.pdf_file_path = pdf_file_path
-    self.get_features()
+  # advanced features: diff_prev_x, diff_next_x
+  feature_keys = ['page', 'rel_x', 'rel_y', 'text', 'len', 'section_idx', 'end_idx', 'has_6_digits', 'x', 'y']
+  default_Y_value = "none"
 
-  def get_features(self):
-    reader = PdfReader("/Users/jeehwancho/repos/pdf_extractor/assets/PM1-815a-single.pdf")
-    reader = PdfReader(self.pdf_file_path)
-    page_number = -1
+  def __init__(self, feature_keys_to_use):
+    self.feature_keys_to_use = feature_keys_to_use
+    self.clf_rf = RandomForestClassifier(n_estimators = 100, max_features = "sqrt", class_weight = {'none': 1, 'ss': 10, 'se': 10})
+    self.clf_dt = DecisionTreeClassifier(max_features = "sqrt", class_weight = {'none': 1, 'ss': 10, 'se': 10})
+
+  def train(self, training_csv_folder):
+    filtered_X, Y = Specification.get_XY_from_folder(training_csv_folder, self.feature_keys_to_use)
+    self.clf_rf = self.clf_rf.fit(filtered_X, Y)
+    self.clf_dt = self.clf_dt.fit(filtered_X, Y)
+
+  def score(self, training_csv_folder):
+    X, Y = Specification.get_XY_from_folder(training_csv_folder, self.feature_keys_to_use)
+    cv = ShuffleSplit(n_splits=5, test_size=0.25, random_state=0)
+    # scores = cross_val_score(self.clf, X, Y, cv=5, scoring = 'f1_macro')
+    scores = cross_val_score(self.clf_rf, X, Y, cv=cv)
+    print(f'Random Forest: {round(sum(scores)/len(scores)*100, 3)}')
+    print(scores)
+    scores = cross_val_score(self.clf_dt, X, Y, cv=cv)
+    print(f'Decision Tree: {round(sum(scores)/len(scores)*100, 3)}')
+    print(scores)
+
+  def predict(self, test_pdf_path, result_csv_path):
+    features_dictionary = Specification.get_features_dictionary(test_pdf_path)
+    filtered_X = Specification.get_X_from_features_dictionary(features_dictionary, self.feature_keys_to_use)
+    Y = self.clf_rf.predict(filtered_X)
+    fully_featured_X = Specification.get_X_from_features_dictionary(features_dictionary)
+    Specification.export_to_csv_from_XY(result_csv_path, fully_featured_X, Y)
+
+  def visualize(self):
+    dot_data = tree.export_graphviz(self.clf_dt, out_file=None,
+                      feature_names=self.feature_keys_to_use,
+                      class_names=['none', 'ss', 'se'],
+                      filled=True, rounded=True,
+                      special_characters=True)
+    graph = graphviz.Source(dot_data)
+    graph.render("assets/tree_result")
+
+  @staticmethod
+  def get_XY_from_folder(training_csv_folder, feature_keys_to_use):
+    res_X = []
+    res_Y = []
+    for file_name in os.listdir(training_csv_folder):
+      if file_name.endswith(".csv"):
+        X, Y = Specification.get_XY_from_csv(f'{training_csv_folder}/{file_name}', feature_keys_to_use)
+        res_X.extend(X)
+        res_Y.extend(Y)
+    return res_X, res_Y
+
+  @staticmethod
+  def export_to_csv(pdf_path, csv_path):
+    features_dictionary = Specification.get_features_dictionary(pdf_path)
+    fully_featured_X = Specification.get_X_from_features_dictionary(features_dictionary)
+    Specification.export_to_csv_from_XY(csv_path, fully_featured_X)
+
+  @staticmethod
+  def export_to_csv_from_XY(csv_path, X, Y = None):
+    with open(csv_path, 'w', newline='') as csvfile:
+      fieldnames = ['Y']
+      fieldnames.extend(Specification.feature_keys)
+      w = csv.DictWriter(csvfile, delimiter='\t', fieldnames=fieldnames)
+      w.writeheader()
+      for i, _ in enumerate(X):
+        w.writerow({
+          'Y': (Specification.default_Y_value if Y is None else Y[i]),
+          'page': X[i][0],
+          'rel_x': X[i][1],
+          'rel_y': X[i][2],
+          'text': X[i][3],
+          'len': X[i][4],
+          'section_idx': X[i][5],
+          'end_idx': X[i][6],
+          'has_6_digits': X[i][7],
+          'x': X[i][8],
+          'y': X[i][9],
+          })
+
+  @staticmethod
+  def get_features_dictionary(pdf_path):
+    features_dictionary = {}
+    reader = PdfReader(pdf_path)
+    current_page_number = -1
+    current_page = None
     def visitor_body(text, cm, tm, fontDict, fontSize):
       x = tm[4]
       y = tm[5]
-      if text == "" or text == "\n":
-        return
-      if y in self.yToFeatures:
-        self.yToFeatures[y]['text'] += text
-      else:
-        self.yToFeatures[y] = {
-          'x': x,
-          'y': y,
-          'page_number': page_number,
-          'text': text,
-        }
+      box = current_page.cropbox
+      rel_x = int((x-float(box.left))/(float(box.right)-float(box.left))*100)
+      rel_y = int((float(box.top)-y)/(float(box.top)-float(box.bottom))*100)
+      for t in text.split("\n"):
+        new_line_counter = Specification.get_new_line_counter(features_dictionary, current_page_number, rel_y)
+        key = Specification.get_features_dictionary_key(current_page_number, rel_y, new_line_counter)
+        if key in features_dictionary:
+          features_dictionary[key]['text'] += t
+        else:
+          features_dictionary[key] = {
+            'page': current_page_number,
+            'rel_x': rel_x,
+            'rel_y': rel_y,
+            'text': t,
+            'x': x,
+            'y': y,
+          }
+          Specification.increment_new_line_counter(features_dictionary, current_page_number, rel_y)
     for i, page in enumerate(reader.pages):
-      page_number = i + 1
+      current_page = page
+      current_page_number = i + 1
       page.extract_text(visitor_text=visitor_body)
-    self.clean()
+    return Specification.post_process_features_dictionary(features_dictionary)
 
-  def export_to_csv(self, csv_file_path):
-    with open(csv_file_path, 'w', newline='') as csvfile:
-      w = csv.DictWriter(csvfile, delimiter='\t', fieldnames=['Y', 'x', 'y', 'text', 'page_number'])
-      w.writeheader()
-      for key in self.yToFeatures:
-        features = self.yToFeatures[key]
-        w.writerow({
-          'Y': '0',
-          'x': features['x'],
-          'y': features['y'],
-          'text': features['text'],
-          'page_number': features['page_number'],
-          })
+  @staticmethod
+  def get_features_dictionary_key(page_number, y, extra):
+    return f'{page_number}-{int(y)}-{extra}'
 
-  def import_csv(self, csv_file_path, feature_keys):
+  @staticmethod
+  def get_new_line_counter(features_dictionary, page_number, y):
+    key = Specification.get_features_dictionary_key(page_number, y, 0)
+    return features_dictionary[key]['new_line_count'] if key in features_dictionary else 0
+
+  @staticmethod
+  def increment_new_line_counter(features_dictionary, page_number, y):
+    key = Specification.get_features_dictionary_key(page_number, y, 0)
+    if 'new_line_count' in features_dictionary[key]:
+      features_dictionary[key]['new_line_count'] += 1
+    else:
+      features_dictionary[key]['new_line_count'] = 1
+
+  @staticmethod
+  def post_process_features_dictionary(features_dictionary):
+    keysToBeDeleted = []
+    for key in features_dictionary:
+      features = features_dictionary[key]
+      text = features['text'].strip()
+      if text == "":
+        keysToBeDeleted.append(key)
+      features['text'] = text
+      features['len'] = len(text)
+      features['section_idx'] = Specification.get_index(text, 'section')
+      features['end_idx'] = Specification.get_index(text, 'end')
+      features['has_6_digits'] = 1 if Specification.has_6_digits(text) else -1
+      features_dictionary[key] = features
+    for key in keysToBeDeleted:
+      del features_dictionary[key]
+    return features_dictionary
+
+  @staticmethod
+  def get_XY_from_csv(csv_path, feature_keys_to_use):
     X = []
     Y = []
-    with open(csv_file_path, newline='') as csvfile:
+    with open(csv_path, newline='') as csvfile:
       r = csv.DictReader(csvfile, delimiter='\t')
       for row in r:
         Y.append(row['Y'])
         features = []
-        for feature_key in feature_keys:
+        for feature_key in feature_keys_to_use:
           features.append(row[feature_key])
         X.append(features)
     return X, Y
 
-  def clean(self):
-    keysToBeDeleted = []
-    for key in self.yToFeatures:
-      features = self.yToFeatures[key]
-      features['text'] = features['text'].strip()
-      if features['text'] == "":
-        keysToBeDeleted.append(key)
+  @staticmethod
+  def get_X_from_features_dictionary(features_dictionary, feature_keys_to_use = None):
+    X = []
+    for key in features_dictionary:
+      features = []
+      if feature_keys_to_use is None:
+        for feature_key in Specification.feature_keys:
+          features.append(features_dictionary[key][feature_key])
       else:
-        self.yToFeatures[key] = features
-    for key in keysToBeDeleted:
-      del self.yToFeatures[key]
+        for feature_key in feature_keys_to_use:
+          features.append(features_dictionary[key][feature_key])
+      X.append(features)
+    return X
 
-pdf_file_path = "/Users/jeehwancho/repos/pdf_extractor/assets/PM1-815a-single.pdf"
-csv_file_path = "data1.csv"
-spec = Specification(pdf_file_path)
-spec.export_to_csv(csv_file_path)
-X, Y = spec.import_csv(csv_file_path, ['x', 'y', 'text'])
+  @staticmethod
+  def get_index(str, substr):
+    try:
+      return str.lower().index(substr)
+    except ValueError:
+      return -1
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
-res = clf.predict([[2., 2.]])
-print(res)
+  @staticmethod
+  def has_6_digits(str):
+    count = 0
+    for ch in str:
+      if ch.isdigit():
+        count += 1
+    return count == 6
 
-# Scikit-learn: https://scikit-learn.org/stable/modules/tree.html
-# X: [[x, y, text]]
-# Y: 0 = nothing, 1 = beginning of section, 2 = end of section
+feature_keys_to_use = ['len', 'section_idx', 'end_idx', 'rel_x', 'rel_y', 'has_6_digits']
+training_csv_path = "assets/training_data.csv"
+training_csv_folder = "/Users/jeehwancho/repos/pdf_extractor/assets/training_csvs"
+test_pdf_path = "/Users/jeehwancho/repos/pdf_extractor/assets/test_pdfs/JW_MARRIOTT_SPECIFICATIONS.pdf"
+result_csv_path = "assets/result.csv"
 
-# TODO:
-# (0) git init
-# (0) use page.mediabox to get relative x, y
-# (1) generate csv for PM1-815a-single.pdf
-# (2) generate csv for PM1-815a.pdf, and feed X, evaluate Y
+# Specification.export_to_csv(training_pdf_path, training_csv_path)
+spec = Specification(feature_keys_to_use) # train based on csv
+spec.score(training_csv_folder)
+spec.train(training_csv_folder)
+spec.predict(test_pdf_path, result_csv_path) # generate csv
+spec.visualize()
+print("done.")
--- readme.md
+++ readme.md
@@ -1,17 +1,31 @@
-Scope out the pdf ml proj
+## Machine Learning & Specification Section Extraction
 
-Features per text: x, y, page, delta x, part (header, footer, body), row in part, etc (possibly add prev/next relations)
+### Goal
+- Evaluate feasibility the spec section extraction via POC
+- Identify limits
+- Identify ways of integrating into existing products
 
-Want: section starts and ends
+### Notes
+- As a python newbie, this code is not following the python best practices.
+- It's written in python because of the popular ML libraries like sklearn
+- I didn't spend much on comparing different classifiers. I started with decision tree as it can visuallize the decision tree. Then random forest because... it's a forest rather than a tree :D
+- It transforms pdf into csv, because (1) I want to inspect data that's persisted somewhere, (2) there are many csv editors out there (no need to implement FE/UI for POC).
+- Features: x & y coordinates of a text, indices of some key words (e.g., section), and length of a text.
+- Accuracy is >99% via cross-validation with standard configuration (5 splits and 25% test). But the accuracy might not be accurate (to be discussed in limits section)
+- Labels: none, ss (section starts), se (section ends)
+- Test files are not checked into this repo due to confidentiality.
 
-TODO:
-- pdf reader to normalize data
-- find ML/AI library to use
-- find pdfs to test it out (in pdf_exporter_service)
-- implement get_features(text)
-- write sample answers to train
-- start with 1/2 x-validation
-- improve
+### Limits/difficulties
+- The pdf reader used in POC is far from perfect. Time to time it acts out on line breakers (e.g., SOME_TEXT_ABOVE\nSECTION\n123456 instead of SECTION 123456). The x & y coordinates of a text sometime jumps around. Definitely need to evaludate other pdf readers.
+- Manually labelling test data is boring and painful
+- 99% accuracy could be misleading because (1) only 2 pdf files were used (total number of pages is ~2k), (2) distribution of classifications/labels are not well balanced (e.g. the number of start of section is a lot smaller than texts not classified for anything). In that sense we can use a [stratification](https://scikit-learn.org/stable/modules/cross_validation.html#cross-validation-iterators-with-stratification-based-on-class-labels) as cross-validation strategy.
 
-source .venv/bin/activate
-python3 -m pip install matplotlib
+### Possible applications (given that accuracy is stable)
+- Deploy a service that periodically pulls user-confirmed spec sections, and train on them. Keep it in the dark until numbers look good. Eventually it generate recommendations to users if the existing service fails to extract data from specification.
+
+### Tech debts
+- Explore different pdf readers
+- Write a method to update existing csv files when features get updated
+- Refactoring (e.g., properly use class variables/methods)
+- Explore more features and functionalities to support other classifications as necessary
+- Look for options that are python equivalent to npm for javascript or bundler for ruby.
